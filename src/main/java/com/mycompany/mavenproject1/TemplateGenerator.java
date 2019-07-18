fccea4d86e376176dc9f034015f3e911eeb9920d
VenkPi <venkpi@vsp.com>
2019-07-18 16:47:30 -0700

    utility

--- src/main/java/com/mycompany/mavenproject1/TemplateGenerator.java
+++ src/main/java/com/mycompany/mavenproject1/TemplateGenerator.java
@@ -47,47 +47,56 @@ public class TemplateGenerator {
                     + "</head><body><script type=\"text/javascript\" src=\"examples.js\"></script>"
                     + "	<div id='viewport'><div id='hd'/><div id='bd'/><div id='ft'/></div>"
                     + "</body></html>";
-            PrintStream ps[] = new PrintStream[1];
-            PrintStream ps1 = new PrintStream(new File("c:\\users\\VenkPi\\Downloads\\bittxn-" + System.currentTimeMillis() + ".html"));
+            PrintStream ps[] = new PrintStream[2];
+            final StringBuffer fileName = new StringBuffer("c:\\users\\VenkPi\\Downloads\\bittxn-" + System.currentTimeMillis() + ".html");
+            PrintStream ps1 = new PrintStream(new File(fileName.toString()));
+            PrintStream ps123 = new PrintStream(new File("C:\\Users\\venkpi\\Downloads\\win-me.bat"));
             ps[0] = ps1;
+            ps[1] = ps123;
             int count[] = {1};
-            int lineCount=1450;
-            final int sum[]=new int[]{lineCount};
+            int lineCount = Files.readAllLines(Paths.get("C:\\Users\\venkpi\\Downloads\\bittx.html")).size()/2;
+            final int sum[] = new int[]{lineCount};
+            final StringBuffer line1 = new StringBuffer();
             Files.readAllLines(Paths.get("C:\\Users\\venkpi\\Downloads\\bittx.html")).forEach(c -> {
                 System.out.print(c.trim());
                 ps[0].println(c);
+                if (count[0] == 1) {
+                    line1.append(c);
+                }
                 if (count[0] == sum[0]) {
                     try {
-                        ps[0].println("<script type=\"text/javascript\">"
+                        ps[0].println("</ul><script type=\"text/javascript\">"
                                 + "function doMe(val,count){"
                                 + "       fetch(\"https://blockchain.info/rawtx/\"+val+\"?cors=true\").then(r => r.json()).then(m => {document.querySelector(\"textarea#data\"+(count+1)).innerText=JSON.stringify(m);});"
                                 + "}"
                                 + "</script>");
-                        ps[0] = new PrintStream("c:\\users\\VenkPi\\Downloads\\bittxn-" + System.currentTimeMillis() + ".html");
-                        ps[0].println("<table><tbody><tr><th></th><td>Select</td><td>TX</td><td>DATA</td></tr><tr><td colspan=\"2\"><a href=\"#\">Merkle Root:f0f9edbf81a230c8c9339d5350b435a19238c7089c76d589dda007fba00005d5</a></td></tr>");
-                    } catch (FileNotFoundException ex) {
+                        fileName.delete(0, fileName.length()).append("c:\\users\\VenkPi\\Downloads\\bittxn-" + System.currentTimeMillis() + ".html");
+                        ps[0] = new PrintStream(fileName.toString());
+                        //start
+                        File f1 = new File("c:\\users\\VenkPi\\Downloads\\win-ram.bat");
+
+                        ps[1].println("C:\\Users\\venkpi\\Documents\\sendme \"xyz@blogger.com\" \"Bit Txns %Date% %time%\" C:\\Users\\venkpi\\Downloads\\" + fileName.toString() + " \"Root:" + line1 + "\"");
+
+                        //end
+                        ps[0].println(line1.toString());
+                    } catch (Exception ex) {
                         Logger.getLogger(TemplateGenerator.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                     sum[0]=sum[0]+lineCount;
+                    sum[0] = sum[0] + lineCount;
                 }
                 count[0]++;
-               
-//                ps.print(c.split("jsonLine=")[1].substring(5).replaceAll("ClaimProcessor.process()  -", "").trim());
 
+//                ps.print(c.split("jsonLine=")[1].substring(5).replaceAll("ClaimProcessor.process()  -", "").trim());
             });
-            ps[0].println("<script type=\"text/javascript\">"
+            ps[0].println("</ul><script type=\"text/javascript\">"
                     + "function doMe(val,count){"
                     + "       fetch(\"https://blockchain.info/rawtx/\"+val+\"?cors=true\").then(r => r.json()).then(m => {document.querySelector(\"textarea#data\"+count).innerText=JSON.stringify(m);});"
                     + "}"
                     + "</script>");
+            ps[1].println("C:\\Users\\venkpi\\Documents\\sendme \"xyz@blogger.com\" \"Bit Txns %Date% %time%\" C:\\Users\\venkpi\\Downloads\\" + fileName + " \"Root:" + line1 + "\"");
 //            ps.print(DATA);
 //            ps.close();
-            File f = new File("c:\\users\\VenkPi\\Downloads");
-            Arrays.asList(f.list()).stream().forEach(ce -> {
-                if (ce.endsWith(".css")) {
-                    System.out.println("<link type='text/css' href='" + ce + "'></link>");
-                }
-            });
+
         } catch (IOException ex) {
             Logger.getLogger(TemplateGenerator.class.getName()).log(Level.SEVERE, null, ex);
         }
